74ce68e4f177711d8bf725652d1d60985de0c512
Your Name <subratmodak3491@gmail.com>
2021-08-06 12:20:43 +0600

    number

--- sumArray.js
+++ sumArray.js
@@ -1,8 +1,18 @@
 var a = [60, 80, 90, 100];
-var sum = 0;
-for (var i = 0; i < a.length; i++) {
-    sum = sum + a[i];
+//var sum = 0;
+//for (var i = 0; i < a.length; i++) {
+//    sum = sum + a[i];
+
+//}
+
+//console.log(sum);
+function numberArray(a) {
+    var s = 0;
+    for (var i = 0; i < a.length; i++) {
+        s = s + a[i];
+    }
+    return s;
 
 }
-
-console.log(sum);
+var sum = numberArray([80, 90, 9, 70, 60]);
+console.log('This is my sum value :', sum);
